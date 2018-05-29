343cf37d4a0472feae48e82a6002e66122b5a1d2
Rich E <rich.eakin@gmail.com>
2018-05-28 23:59:14 -0400

    testing opening a non-default callback stream

--- test/PortAudioRepoExamples/src/PortAudioRepoExamplesApp.cpp
+++ test/PortAudioRepoExamples/src/PortAudioRepoExamplesApp.cpp
@@ -2,6 +2,7 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Log.h"
+#include "cinder/Rand.h"
 
 #include "portaudio.h"
 
@@ -14,17 +15,23 @@ using namespace std;
 class PortAudioRepoExamplesApp : public App {
   public:
 	void setup() override;
-	void printInfo();
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+	void printInfo();
+	void testOpenStream();
+
+	PaStream *mStream = nullptr;
 };
 
 void PortAudioRepoExamplesApp::setup()
 {
-	//paex_saw_main();
+	//printInfo();
 
-	printInfo();
+	//paex_saw_main();
+	
+	testOpenStream();
 }
 
 void PortAudioRepoExamplesApp::printInfo()
@@ -48,14 +55,72 @@ void PortAudioRepoExamplesApp::printInfo()
 	for( PaDeviceIndex i = 0; i < numDevices; i++ ) {
 		auto dev = Pa_GetDeviceInfo( i );
 		CI_LOG_I( "device index: " << i << ", host api: " << dev->hostApi << ", name: " << dev->name << ", input channels: " << dev->maxInputChannels
-			<< ", output channels" << dev->maxOutputChannels << ", samplerate: " << dev->defaultSampleRate
+			<< ", output channels: " << dev->maxOutputChannels << ", samplerate: " << dev->defaultSampleRate
 			<< ", latency (low): [" << dev->defaultLowInputLatency << ", " << dev->defaultLowOutputLatency << "]"
 			<< ", latency (high): [" << dev->defaultHighInputLatency << ", " << dev->defaultHighOutputLatency << "]" );
 	}
+
+	CI_LOG_I( "default input: " << Pa_GetDefaultInputDevice() << ", default output: " << Pa_GetDefaultOutputDevice() );
+}
+
+static int noiseCallback( const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData )
+{
+	float *out = (float*)outputBuffer;
+	const float *in = (const float *)inputBuffer;
+
+	(void) timeInfo; /* Prevent unused variable warnings. */
+	(void) statusFlags;
+	(void) userData;
+
+	const float vol = 0.1f;
+	for( int i = 0; i < framesPerBuffer; i++ ) {
+		*out++ = ci::randFloat( -vol, vol ); // left
+		*out++ = ci::randFloat( -vol, vol ); // right
+	}
+
+	return paContinue;
+}
+
+void PortAudioRepoExamplesApp::testOpenStream()
+{
+	PaDeviceIndex realtekWasapiIndex = 2;
+	const unsigned long framesPerBuffer = 256;
+
+	PaError err = Pa_Initialize();
+	CI_ASSERT( err == paNoError );
+
+	auto devInfo = Pa_GetDeviceInfo( realtekWasapiIndex );
+
+	// Open an audio I/O stream.
+	PaStreamParameters outputParams;
+	outputParams.device = realtekWasapiIndex;
+	outputParams.channelCount = devInfo->maxOutputChannels;
+	outputParams.sampleFormat = paFloat32;
+	outputParams.suggestedLatency = devInfo->defaultLowInputLatency;
+	outputParams.hostApiSpecificStreamInfo = NULL;
+
+
+
+	PaStreamFlags flags = 0;
+	//PaError err = Pa_OpenDefaultStream( &stream, 0, 2, paFloat32, 44100, 256, patestCallback, &data );
+	err = Pa_OpenStream( &mStream, nullptr, &outputParams, devInfo->defaultSampleRate, framesPerBuffer, flags, noiseCallback, this );
+	CI_ASSERT( err == paNoError );
+
+	err = Pa_StartStream( mStream );
+	CI_ASSERT( err == paNoError );
 }
 
 void PortAudioRepoExamplesApp::mouseDown( MouseEvent event )
 {
+	if( ! mStream )
+		return;
+
+	CI_LOG_I( "closing stream.." );
+
+	PaError err = Pa_CloseStream( mStream );
+	CI_ASSERT( err == paNoError );
+
+	mStream = nullptr;
 }
 
 void PortAudioRepoExamplesApp::update()
